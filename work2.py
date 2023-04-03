96dc03fef52ffc011a99a251be05380eeb0419d5
Yuji Tokiwa <tokiwa@yujitokiwa.jp>
2023-04-03 22:26:32 +0900

    Initial commit

--- work2.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request
-app = Flask(__name__)
-@app.route('/')
-def get_request():
-    contents = request.args.get('value', '')
-    return contents
-if __name__ == "__main__":
-    app.run(debug=True)
